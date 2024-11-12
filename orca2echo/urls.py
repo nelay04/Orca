ab6e008a93ca34a2d503f60bdf9db11138511328
nelay-tw <145161848+nelay2004@users.noreply.github.com>
2024-11-12 17:56:03 +0530

    remove redundant url

--- orca2echo/urls.py
+++ orca2echo/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.index, name="orca"),     # route to index function of views.py home
     path('mobile-only/', views.mobile_only, name='mobile_only'),
     path("signin", views.signin, name="signin"),
-    path('send-geolocation/', views.send_geolocation, name='send_geolocation'),
+    
     
     # path("about", views.about, name="about"),
     # path("recommendation", views.recommendation, name="recommendation"),
