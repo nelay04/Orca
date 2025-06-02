7f6da13200247da6548a7f54f11eed96b37e4195
nelay04 <nelaykarmakar28@gmail.com>
2025-06-02 13:15:46 +0530

    Updating code

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+# Set default credentials (not secure)
+DJANGO_SUPERUSER_USERNAME=admin-rover
+DJANGO_SUPERUSER_EMAIL=info@sagerover.com
+DJANGO_SUPERUSER_PASSWORD=Sr2025
+
+
+# Run Django migrations
+python manage.py migrate --noinput
+
+# Collect static files into the STATIC_ROOT directory
+python manage.py collectstatic --noinput
+
+
+# Create a superuser with the specified username and password
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+"
+
+# Start the app using Gunicorn and the WSGI server
+# exec gunicorn --bind 0.0.0.0:8004 orca.wsgi:application
+
+# exec uvicorn orca.asgi:application --reload --host 127.0.0.1 --port 8004
+exec uvicorn orca.asgi:application --reload --host 0.0.0.0 --port 8004
